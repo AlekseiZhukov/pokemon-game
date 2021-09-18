068b05cedfdaeab68b6303eeac78f7fe4a92990d
AlekseiZhukov <alex__zhukov@mail.ru>
2021-09-18 20:13:37 +0300

    homewokr6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {useRouteMatch, Route, Switch, Redirect} from 'react-router-dom'
 import cn from 'classnames'
-import HomePage from "./routes/Home";
-import GamePage from "./routes/Game";
-import NotFoundPage from "./routes/NotFound";
-import MenuHeader from "./components/MenuHeader";
-import Footer from "./components/Footer";
+import HomePage from "./routes/Home"
+import GamePage from "./routes/Game"
+import NotFoundPage from "./routes/NotFound"
+import MenuHeader from "./components/MenuHeader"
+import Footer from "./components/Footer"
+import AboutPage from "./routes/AboutPage"
+import ContactPage from "./routes/ContactPage"
+
 import './App.css'
 import s from './style.module.css'
-import AboutPage from "./routes/AboutPage";
-import ContactPage from "./routes/ContactPage";
+
 
 const App = () => {
 
--- src/routes/Game/index.jsx
+++ src/routes/Game/index.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {useHistory} from "react-router-dom";
-import {pokemons} from '../../data/pokemons'
+import database from '../../service/firebase'
+//import {pokemons} from '../../data/pokemons'
 import PokemonCard from "../../components/PokemonCard";
 
 
@@ -8,13 +9,66 @@ import PokemonCard from "../../components/PokemonCard";
 const GamePage = () => {
 
     const history = useHistory()
-    const [pokemonsData, setPokemonsData] = useState(pokemons)
+    const [pokemonsData, setPokemonsData] = useState({})
+    const [keyPokemonIsActive, setKeyPokemonIsActive] = useState('')
+    const [addedNewPokemon, setAddedNewPokemon] = useState(false)
+
+    const getDataPokemons = () => {
+        database.ref('pokemons').once('value', (snapshot) => {
+            setPokemonsData(snapshot.val())
+        })
+    }
+
+
+    useEffect(() => {
+        getDataPokemons()
+    }, [])
+
+
+    useEffect(() => {
+    const modifiedPokemon = Object.entries(pokemonsData).filter(([key]) => key === keyPokemonIsActive).reduce((acc, el) => (acc[el[0]] = el[1]), {})
+        if (Object.keys(modifiedPokemon).length > 0) {
+            database.ref('pokemons/' + keyPokemonIsActive).set(modifiedPokemon)
+        }
+
+    }, [pokemonsData, keyPokemonIsActive])
+
+    useEffect( () => {
+        getDataPokemons()
+    },[addedNewPokemon])
 
     const onchangeActiveCard = (id) => {
 
-        setPokemonsData(prevState => prevState.map(p => p.id === id ? {...p, active: !p.active} : p))
+        setPokemonsData(prevState => {
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = {...item[1]}
+                if (pokemon.id === id) {
+                    setKeyPokemonIsActive(`${item[0]}`)
+                    pokemon.active ? pokemon.active = false : pokemon.active = true
+
+                }
+                acc[item[0]] = pokemon
+
+                return acc
+            }, {})
+
+        })
 
     }
+
+    const handleClickButton = () => {
+        const dataArray = Object.entries(pokemonsData)
+        const indexArray = Math.floor(Math.random() * (dataArray.length))
+        if (dataArray.length > 0) {
+            const data = dataArray[indexArray][1]
+            const newKey = database.ref().child('pokemons').push().key;
+            database.ref('pokemons/' + newKey).set(data);
+
+            setAddedNewPokemon(prewState => !prewState)
+        }
+    }
+
+
     const handleClick = () => {
         history.push('/')
     }
@@ -26,19 +80,22 @@ const GamePage = () => {
                 <div>
                     <button onClick={handleClick}>return Home Page</button>
                 </div>
+                <div>
+                    <button onClick={handleClickButton}>доавить покемона</button>
+                </div>
                 <h1>
                     This is Game Page!!!
                 </h1>
                 <div className="flex">
                 {
-                    pokemonsData.map(item =>  <PokemonCard
-                            key={item.id}
-                            name={item.name}
-                            img={item.img}
-                            id={item.id}
-                            type={item.type}
-                            values={item.values}
-                            isActive={item.active}
+                    Object.entries(pokemonsData).map(([key, {name, img, id, type, values, active}]) =>  <PokemonCard
+                            key={key}
+                            name={name}
+                            img={img}
+                            id={id}
+                            type={type}
+                            values={values}
+                            isActive={active}
                             onchangeActiveCard={onchangeActiveCard}
                         />
                     )
